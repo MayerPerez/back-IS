60feb4ba8a859f34286d62bd3af13a7514f1fa1e
Mayer Abraham Pérez González <38333546+MayerPerez@users.noreply.github.com>
2023-01-02 23:19:00 -0600

    test db connection

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use Exception;
 use Throwable;
 use Validator;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Traits\ResponseApi;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+
+/*
+uncomment
+extension=pdo_mysql
+restart server
+*/
 
 class TestController extends Controller
 {
@@ -22,9 +30,50 @@ class TestController extends Controller
     public function test()
     {
         try {
-            
-            return $this->sendResponse(true, 'Response');
+            if(DB::connection()->getDatabaseName()) {
+                $message = "Yes! successfully connected to the DB: " . DB::connection()->getDatabaseName();
+            } else {
+                $message = "Error in connection";
+            }
+            return $this->sendResponse($message, 'Response');
         } catch (\Exception $e) {
+            return $this->sendError('TestController test', $e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function countTables()
+    {
+        try {
+            $count = DB::table('users')->count();
+            return $this->sendResponse($count, 'Response');
+        } catch (\Exception $e) {
+            Log::info($e);
+            return $this->sendError('TestController test', $e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function index()
+    {
+        try {
+            $users = DB::table('users')->get();
+            return $this->sendResponse($users, 'Response');
+        } catch (\Exception $e) {
+            Log::info($e);
+            return $this->sendError('TestController test', $e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $user = new User();
+            $user->name = "Prueba1";
+            $user->email = "prueba@test.com";
+            $user->password = "prueba@test.com";
+            $user->save();
+            return $this->sendResponse($user, 'Response');
+        } catch (\Exception $e) {
+            Log::info($e);
             return $this->sendError('TestController test', $e->getMessage(), $e->getCode());
         }
     }
